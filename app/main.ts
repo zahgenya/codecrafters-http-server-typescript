9ea75930995630cf1001165f0cd94c9c4f779bf0
Zhenek Zakharchenko <zahgenya@gmail.com>
2024-06-04 22:38:22 +0300

    Added not found handling

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,17 @@
 import * as net from 'net';
 
 const server = net.createServer((socket) => {
-  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`))
-  socket.end();
+  socket.on("data", (data) => {
+    const req = data.toString()
+    const path = req.split(' ')[1]
+    console.log(req)
+    if (path === '/') {
+      socket.write('HTTP/1.1 200 OK\r\n\r\n')
+    } else {
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+    }
+    socket.end()
+  })
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,5 +19,5 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 server.listen(4221, 'localhost', () => {
-    console.log('Server is running on port 4221');
+  console.log('Server is running on port 4221');
 });
