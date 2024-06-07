f5e599f0abaafad32210ffacf0fdcaf83694ffa3
Zhenek Zakharchenko <zahgenya@gmail.com>
2024-06-07 22:20:44 +0300

    Added encoding header (without encoding for now), and new class responseBuilder

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,19 @@
-import * as net from 'net';
-import * as fs from 'fs/promises'
+import * as net from "net";
+import * as fs from "fs/promises"
 
-type RouterHandler = (method: string, params: { [key: string]: string }, usrAgent?: string, reqBody?: string) => Promise<string>
+type RouterHandler = (method: string, params: { [key: string]: string }, usrAgent?: string, reqBody?: string, encoding?: string) => Promise<string>
 
 type Route = {
   pattern: RegExp;
   handler: RouterHandler;
+}
+
+const statuses: { [key: number]: string } = {
+  200: "OK",
+  405: "Method Not Allowed",
+  404: "Not Found",
+  201: "Created",
+  500: "Internal Server Error",
 }
 
 class Router {
@@ -15,9 +23,9 @@ class Router {
     this.routes.push({ pattern, handler })
   }
 
-  async handleReq(method: string, path: string, usrAgent?: string, reqBody?: string): Promise<string | undefined> {
+  async handleReq(method: string, path: string, usrAgent?: string, reqBody?: string, encoding?: string): Promise<string | undefined> {
     if (!path) {
-      console.error('Path is not defined!')
+      console.error("Path is not defined!")
       return undefined
     }
     for (const route of this.routes) {
@@ -26,7 +34,7 @@ class Router {
       if (match) {
         const params = match.groups || {}
         console.log(params)
-        return await route.handler(method, params, usrAgent, reqBody)
+        return await route.handler(method, params, usrAgent, reqBody, encoding)
       }
     }
     console.log("No route matched")
@@ -36,84 +44,136 @@ class Router {
 
 const router = new Router()
 
+class responseBuilder {
+  private statusCode: number = 0;
+  private contentBody: string = "";
+  private headers: { [key: string]: string } = {};
+  private clrf: string = "\r\n";
+
+  status(code: number): this {
+    this.statusCode = code
+    return this
+  }
+
+  header(key: string, value: string): this {
+    this.headers[key] = value
+    return this
+  }
+
+  body(content: string): this {
+    this.contentBody = content
+    return this
+  }
+
+  buildResponse(acceptEncoding?: string): string {
+    console.log("Encoding inside builder: ", acceptEncoding)
+    if (acceptEncoding?.includes("gzip")) {
+      this.header("Content-Encoding", "gzip")
+    }
+    const statusMsg = statuses[this.statusCode] || "Unknown Status"
+    const headers = Object.entries(this.headers)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join(this.clrf)
+    const responseStr = `HTTP/1.1 ${this.statusCode} ${statusMsg}${this.clrf}`
+    return `${responseStr}${headers}${this.clrf}${this.clrf}${this.contentBody}`
+  }
+}
 
 // ROUTES:
 router.addRoute(/^\/$/, async (method) => {
   if (method === "GET") {
-    return `HTTP/1.1 200 OK\r\n\r\n`
+    return new responseBuilder().status(200).buildResponse()
   }
-  return `HTTP/1.1 405 Method Not Allowed\r\n\r\n`
+  return new responseBuilder().status(405).buildResponse()
 })
-router.addRoute(/^\/echo\/(?<msg>.+)$/, async (method, params) => {
+router.addRoute(/^\/echo\/(?<msg>.+)$/, async (method, params, userAgent, reqBody, encoding) => {
   if (method === "GET") {
-    return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${params.msg.length}\r\n\r\n${params.msg}`
+    return new responseBuilder()
+      .status(200)
+      .header("Content-Type", "text/plain")
+      .header("Content-Length", params.msg.length.toString())
+      .body(params.msg)
+      .buildResponse(encoding)
   }
-  return `HTTP/1.1 405 Method Not Allowed\r\n\r\n`
+  return new responseBuilder().status(405).buildResponse()
 })
-router.addRoute(/^\/user-agent$/, async (method, params, usrAgent) => {
-  const res = usrAgent || 'No User-Agent provided'
+router.addRoute(/^\/user-agent$/, async (method, params, userAgent, reqBody, encoding) => {
+  const res = userAgent || "No User-Agent provided"
   if (method === "GET") {
-    return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}`
+    return new responseBuilder()
+      .status(200)
+      .header("Content-Type", "text/plain")
+      .header("Content-Length", params.res.length.toString())
+      .body(res)
+      .buildResponse(encoding)
   }
-  return `HTTP/1.1 405 Method Not Allowed\r\n\r\n`
+  return new responseBuilder().status(405).buildResponse()
 })
 
 const baseDir = process.argv[3]
 
-router.addRoute(/^\/files\/(?<file>.+)$/, async (method, params, userAgent, reqBody) => {
+router.addRoute(/^\/files\/(?<file>.+)$/, async (method, params, userAgent, reqBody, encoding) => {
   if (method === "GET") {
     try {
       const path = `${baseDir}${params.file}`
       console.log(`baseDir: ${baseDir}, path: ${path}`)
-      const fileData = await fs.readFile(path, 'utf-8')
-      return `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileData.length}\r\n\r\n${fileData}`
+      const fileData = await fs.readFile(path, "utf-8")
+      return new responseBuilder()
+        .status(200)
+        .header("Content-Type", "application/octet-stream")
+        .header("Content-Length", fileData.length.toString())
+        .body(fileData)
+        .buildResponse(encoding)
     } catch (err) {
       console.log("Error has occurred while reading file: ", err)
-      return `HTTP/1.1 404 Not Found\r\n\r\n`
+      return new responseBuilder().status(404).buildResponse()
     }
   } else if (method === "POST") {
-    const reqMsg = reqBody || 'Request body is empty'
+    const reqMsg = reqBody || "Request body is empty"
     const path = `${baseDir}${params.file}`
     try {
       await fs.writeFile(path, reqMsg)
-      return `HTTP/1.1 201 Created\r\n\r\n`
+      return new responseBuilder().status(201).buildResponse()
     } catch (err) {
       console.log("Error has occurred while writting a file: ", err)
-      return `HTTP/1.1 500 Internal Server Error\r\n\r\n`
+      return new responseBuilder().status(500).buildResponse()
     }
   }
-  return `HTTP/1.1 405 Method Not Allowed\r\n\r\n`
+  return new responseBuilder().status(405).buildResponse()
 })
 
 const server = net.createServer((socket) => {
   socket.on("data", async (data) => {
     try {
       const req = data.toString()
-      const path = req.split(' ')[1]
-      const method = req.split(' ')[0].trim()
-      const reqBody = req.split('\r\n\r\n')[1];
-      const usrAgentHeader = req.split('\n').find(line => line.startsWith('User-Agent:'))
+      const path = req.split(" ")[1]
+      const method = req.split(" ")[0].trim()
+      const reqBody = req.split("\r\n\r\n")[1];
+      const usrAgentHeader = req.split("\n").find(line => line.startsWith("User-Agent:"))
+      const encodingHeader = req.split("\n").find(line => line.startsWith("Accept-Encoding:"))
+      const encoding = encodingHeader ? encodingHeader.slice(17).trim() : undefined
+      console.log(`ENCODING=(${encoding})`)
       const usrAgent = usrAgentHeader ? usrAgentHeader.slice(12).trim() : undefined
       console.log(req)
-      let res = await router.handleReq(method, path, usrAgent, reqBody)
+      let res = await router.handleReq(method, path, usrAgent, reqBody, encoding)
       if (res !== undefined) {
         socket.write(res)
       } else {
-        socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`)
+        socket.write(new responseBuilder().status(404).buildResponse())
       }
     } catch (err) {
       console.error("Error processing request!")
-      socket.write(`HTTP/1.1 500 Internal Server Error\r\n\r\n`)
+      socket.write(new responseBuilder().status(500).buildResponse())
     } finally {
       socket.end()
     }
   })
 });
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
+// You can use print statements as follows for debugging, they"ll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-server.listen(4221, 'localhost', () => {
-  console.log('Server is running on port 4221');
+server.listen(4221, "localhost", () => {
+  console.log("Server is running on port 4221");
 });
